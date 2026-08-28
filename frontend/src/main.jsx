449c292630f68cf891fce5679a3651db12712139
Snehasish Das <141271526+Unknowncoder3@users.noreply.github.com>
2026-08-28 14:40:36 +0530

    Load authentication styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './styles.css'
+import './auth.css'
 import App from './App'
 ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
